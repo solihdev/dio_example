3abbb417127362d930883c7b22fcf397dc4f0efe
Abdumutalibovo <odilbekjuraev07@gmail.com>
2022-12-20 18:43:53 +0500

    add sample page done

--- lib/ui/sample_usage.dart
+++ lib/ui/sample_usage.dart
@@ -17,25 +17,82 @@ class _SampleUsageState extends State<SampleUsage> {
         title: const Text("Simple Usage"),
       ),
       body: Consumer<ProductViewModel>(
-        builder: (context,viewModel,child){
-          if(viewModel.productModel==null){
+        builder: (context, viewModel, child) {
+          if (viewModel.productModel == null) {
             return Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Center(child: Text("No data Yet"),),
-                ElevatedButton(onPressed: () {
-                  context.read<ProductViewModel>().fetchProduct();
-                }, child: const Text("Get Data")),
+                const Center(
+                  child: Text("No data Yet"),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      context.read<ProductViewModel>().fetchProduct();
+                    },
+                    child: const Text("Get Data")),
               ],
             );
           }
-          return Column(
-            children: [
-              Text(viewModel.productModel![0].id.toString()),
-              Text(viewModel.productModel![0].name),
-              Text(viewModel.productModel![0].price.toString()),
-            ],
-          );
+          return ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount: viewModel.productModel!.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Container(
+                    width: 200,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.grey,
+                          spreadRadius: 1,
+                          blurRadius: 1,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      // crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            SizedBox(
+                              width: 200,
+                              height: 180,
+                              child: Image.network(
+                                viewModel.productModel![index].imageUrl,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Expanded(
+                                child: Text(
+                              viewModel.productModel![index].name,
+                              style: const TextStyle(
+                                fontSize: 28,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            )),
+                            const SizedBox(
+                              width: 40,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              });
         },
       ),
     );
